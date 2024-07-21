b98f4b97ab45d7d7766e42060dbef76fa79bd124
Malak <Malakkassas95@gmail.com>
2024-07-21 15:41:54 +0200

    apimalak

---
+++ backend/controllers/auth/singup.controllers.js
@@ -0,0 +1,54 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../../models/User.js';
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User'); // Adjust the path as necessary
+
+exports.signUp = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Check If The Input Fields are Valid
+        if (!username || !password || !email) {
+            return res
+                .status(400)
+                .json({ message: "Please Input Username and Password and email" });
+        }
+
+        // Check If User Exists In The Database
+        const existingUser = await User.findOne({ email: req.body.email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        // Hash The User's Password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Save The User To The Database
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        // Generate JWT Token
+        const token = jwt.sign(
+            { userId: newUser._id, email: newUser.email },
+            'your_secret_key', // Replace with your secret key
+            { expiresIn: '1h' } // Token expiration time
+        );
+
+        return res
+            .status(201)
+            .json({ message: "User Created Successfully", token });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: "Error creating user" });
+    }
+};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { login } from '../controllers/auth/login.controller.js';
+import { singup } from '../controllers/auth/singup.controllers.js';
 
 const router = express.Router();
 
 router.put('/', login);
-// router.post('/', register); 
+router.post('/',singup);
 
 export default router;
