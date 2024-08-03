52867fbcec7d7577cec28f08a18818d97ceffea8
jaber <jaberlweis@gmail.com>
2024-08-03 11:06:02 +0300

    new form login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div className="w-full" >
       <h1 className="text-2xl">Home Page</h1>
-      {/* <Login /> */}
+      <Login />
       {/* <Join /> */}
       {/* <Portfolio /> */}
-      <Signup />
+      {/* <Signup /> */}
     </div>
   );
 };
