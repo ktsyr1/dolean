f86f361a048c7021ca0e8230d1c3a4b46ebd501f
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-08-05 18:10:15 +0300

    app

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,8 +7,8 @@ let Hero = () => {
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
 
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the worlds potential</h1>
-                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovatizon, and capital can unlock long-term value and drive economic growth.</p>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">قم بالوصول الى الدورات التي تحتاجها في لبنان</h1>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">قم بتزويد فرقك بالتدريب على أحدث استراتيجيات التكنولوجيا والأعمال والقيادة.</p>
                 <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     <Link to="/apply" className="inline-flex mx-4 justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"> اخبرنا عنك </Link>
                     <Link href="/apply" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center bg-white border border-primary-700 rounded-lg text-primary-700 hover:text-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"> تسجيل دخول </Link>
