f2a35438b7779fd6f4d709facadb8bc88813900a
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-08-14 13:13:02 +0300

    layout admin clean

--- client/src/app/admin/layoutAdmin.jsx
+++ client/src/app/admin/layoutAdmin.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import Login from "../../components/forms-client/login";
-import Signup from "../../components/forms-client/signup";
 
 const Admin = () => {
     return (
         <div>
             <p> لوحة التحكم </p>
-            {/* اصتيراد صفحات لوحة التحكم هنا */}
+            {/* استيراد صفحات لوحة التحكم هنا */}
         </div>
     )
 };
