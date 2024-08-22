9142618cfaf99b4f46cdabb9146052d0da0ff5bd
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2024-08-22 16:31:23 +0300

    test debug

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors()); // السماح بكل الأصول
 
 app.use(express.json());
+app.use(express.static("/api-docs"));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
